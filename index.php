e13f20b650c655d8423d6b288dfc66b2a37cd4ff
jonkofee <jonkofee@icloud.com>
2017-07-28 15:27:30 +0300

    index

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jonkofee
- * Date: 28/07/2017
- * Time: 14:35
- */
+use Phalcon\Loader;
+use Phalcon\Mvc\Application;
+use Phalcon\Di\FactoryDefault;
+
+$debug = (new \Phalcon\Debug())->listen();
+
+$loader = new Loader();
+
+$loader->registerDirs(
+	[
+		'../api/controllers/',
+		'../api/models/',
+		'Helpers/'
+	]
+)->register();
+
+$di = new FactoryDefault();
+
+$application = new Application($di);
+
+try {
+	$response = $application->handle();
+
+	$response->send();
+} catch (\Exception $e) {
+	echo $e->getMessage();
+}
