948af92f16aa4604614a0ad47b129a672c9e1a96
jonkofee <jonkofee@icloud.com>
2017-07-28 15:27:00 +0300

    add debug class

---
+++ Helpers/Debug.php
@@ -0,0 +1,15 @@
+<?php
+
+class Debug
+{
+
+	public static function dumpDie($var)
+	{
+		$objDump = new \Phalcon\Debug\Dump();
+
+		echo "<pre>";
+		echo $objDump->variable($var);
+//		die();
+	}
+
+}
