9f8b587083a40075edd08cb73907c5938e6369d2
jonkofee <jonkofee@icloud.com>
2017-07-28 15:32:21 +0300

    include config file in DI

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 use Phalcon\Loader;
+use Phalcon\Config\Adapter\Ini;
 use Phalcon\Mvc\Application;
 use Phalcon\Di\FactoryDefault;
 
 $di = new FactoryDefault();
-
+$di->set('config', new Ini('api/config/config.ini'));
 $debug = (new \Phalcon\Debug())->listen();
 
 $loader = new Loader();
