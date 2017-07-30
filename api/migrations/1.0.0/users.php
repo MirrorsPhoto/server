5aa8b51c1c2498bc1dca4fe2952985ed99d66bdb
jonkofee <jonkofee@icloud.com>
2017-07-30 14:38:27 +0300

    add migration and seed for `users` table

---
+++ api/migrations/1.0.0/users.php
@@ -0,0 +1,111 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class UsersMigration_100
+ */
+class UsersMigration_100 extends Migration
+{
+	private $_tableName = 'users';
+
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable($this->_tableName, [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'username',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                        ]
+                    ),
+                    new Column(
+                        'first_name',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                        ]
+                    ),
+                    new Column(
+                        'last_name',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                        ]
+                    ),
+                    new Column(
+                        'middle_name',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                        ]
+                    ),
+                    new Column(
+                        'password',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                            'notNull' => true,
+                        ]
+                    ),
+                    new Column(
+                        'email',
+                        [
+                            'type' => Column::TYPE_TEXT,
+                            'notNull' => true,
+                        ]
+                    ),
+                    new Column(
+                        'datetime_create',
+                        [
+                            'type' => Column::TYPE_TIMESTAMP,
+                            'default' => "now()",
+                            'notNull' => true,
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('users_email_key', ['email'], null),
+                    new Index('users_pkey', ['id'], null),
+                    new Index('users_username_key', ['username'], null)
+                ],
+            ]
+        );
+
+
+	    $this->batchInsert('users', [
+			    'id',
+			    'username',
+			    'first_name',
+			    'last_name',
+			    'middle_name',
+			    'password',
+			    'email',
+			    'datetime_create'
+		    ]
+	    );
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+        self::$_connection->dropTable($this->_tableName);
+    }
+
+}
