d54fa5dee07b45c801e3279b5f4a5c4eff4f8b4f
jonkofee <jonkofee@icloud.com>
2017-08-01 15:30:37 +0300

    add singleton trait

---
+++ Core/Singleton.php
@@ -0,0 +1,31 @@
+<?php
+trait Singleton
+{
+	/**
+	 * @var self
+	 */
+	protected static $_objInstance;
+	/**
+	 * @return self
+	 */
+	public static function getInstance()
+	{
+		if (is_null(self::$_objInstance))
+		{
+			self::$_objInstance = new static();
+		}
+		return self::$_objInstance;
+	}
+	/**
+	 * @return self
+	 */
+	public static function refreshInstance()
+	{
+		self::$_objInstance = new self();
+		return self::$_objInstance;
+	}
+	public function __wakeup()
+	{
+		trigger_error('Unserializing ' . __CLASS__ . ' is not allowed.', E_USER_ERROR);
+	}
+}
