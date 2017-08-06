6e8acf0719533085bbd99213cb470bbba5879d1e
jonkofee <jonkofee@icloud.com>
2017-08-06 17:11:46 +0300

    acl done

--- Core/UserCenter/Security.php
+++ Core/UserCenter/Security.php
@@ -2,6 +2,8 @@
 
 namespace Core\UserCenter;
 
+use Core\UserCenter\Exception\AccessDenied;
+use Core\UserCenter\Exception\Unauthorized;
 use Phalcon\Events\Event;
 use Phalcon\Mvc\User\Plugin;
 use Phalcon\Acl;
@@ -56,6 +58,7 @@ class Security extends Plugin
 
 		return $acl;
 	}
+
 	public function beforeExecuteRoute(Event $event, Dispatcher $dispatcher)
 	{
 		$userEnum = Enum::getInstance();
@@ -66,25 +69,31 @@ class Security extends Plugin
 		// Получаем список ACL
 		$acl = $this->_getAcl();
 
-		if ($acl->isAllowed($userEnum->getName($userEnum::GUEST), $controller, $action)) return TRUE;
+		if ($acl->isAllowed($userEnum->getName($userEnum::GUEST), $controller, $action)) return true;
 
-		// Проверяем, установлен ли в сессии user
-//		$isAuth = $this->session->has('user');
-//
-//		//Если не авторизован, но перенаправляем на страницу авторизации
-//		if (!$isAuth)
-//		{
-//			return $this->_forwardToLogin();
-//		}
-//
-//		$user = $this->session->get('user');
-//		$role = $user->type;
-//
-//		// Проверяем, имеет ли данная роль доступ к контроллеру (ресурсу)
-//		$allowed = $acl->isAllowed($userEnum->getName($role), $controller, $action);
-//		if ($allowed != Phalcon\Acl::ALLOW)
-//		{
-//			throw new Core_UserCenter_Exception_AccessDenied($controller, $action, $role);
-//		}
+		$request = $dispatcher->getDI()->get('request');
+
+		if (!$header = $request->getHeader('Authorization')) {
+			$dispatcher->getDI()->get('response')->setHeader('WWW-Authenticate', 'Basic realm="Unauthorized"');
+			throw new Unauthorized();
+		}
+
+		$token = explode(' ', $header)[1];
+
+		if (!$token) {
+			$dispatcher->getDI()->get('response')->setHeader('WWW-Authenticate', 'Basic realm="Bad Unauthorized"');
+			throw new Unauthorized();
+		}
+
+		$decoded = \JWT::decode($token);
+
+		$role = $decoded->type;
+
+		// Проверяем, имеет ли данная роль доступ к контроллеру (ресурсу)
+		$allowed = $acl->isAllowed($userEnum->getName($role), $controller, $action);
+		if ($allowed != Acl::ALLOW)
+		{
+			throw new AccessDenied($controller, $action, $role);
+		}
 	}
 }
