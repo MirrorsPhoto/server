b3f607d0659bf5451ef719d756d51c5da950c56f
jonkofee <jonkofee@icloud.com>
2017-08-06 17:11:38 +0300

    fix

--- Core/JWT.php
+++ Core/JWT.php
@@ -5,12 +5,12 @@ class JWT extends \Firebase\JWT\JWT
 
 	private static $key = 'devkey';
 
-	public static function encode($payload)
+	public static function encode($payload, $key = null, $alg = 'HS256', $keyId = null, $head = null)
 	{
 		return parent::encode($payload, self::$key);
 	}
 
-	public static function decode($jwt)
+	public static function decode($jwt, $key = null, array $allowed_algs = array())
 	{
 		return parent::decode($jwt, self::$key, ['HS256']);
 	}
