a67a209411a5bd784f7bfdf5e820992a43160019
jonkofee <jonkofee@icloud.com>
2017-08-14 17:07:35 +0300

    validate abstract class

---
+++ Core/Validation.php
@@ -0,0 +1,19 @@
+<?php
+
+class Validation extends \Phalcon\Validation
+{
+
+	public function afterValidation($data, $entity, $messages)
+	{
+		if (!$messages->count()) return;
+
+		$errorMessage = '';
+
+		foreach ($messages as $message) {
+			$errorMessage .= $message->getMessage() . '. ';
+		}
+
+		throw new \Core\Exception\BadRequest($errorMessage);
+	}
+
+}
