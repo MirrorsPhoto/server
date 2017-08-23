74bfd2f08f64bf9b72349093c6aa0881a74857c9
jonkofee <jonkofee@icloud.com>
2017-08-23 13:36:39 +0300

    Готовая логика продажи товара

--- api/controllers/GoodController.php
+++ api/controllers/GoodController.php
@@ -6,5 +6,23 @@
 class GoodController extends Controller
 {
 
+	/**
+	 * @Post('/sale')
+	 */
+	public function saleAction()
+	{
+		(new \Validator\Good\Sale())->validate();
+
+		$barCode = $login = $this->getPost('bar_code');
+
+		$good = Good::findFirstByBarCode($barCode);
+
+		if (!$good) throw new \Core\Exception\BadRequest('Товар с таким кодом отсутствует');
+
+		$good->sale();
+
+		return 'Товар успешно продан';
+
+	}
 
 }
