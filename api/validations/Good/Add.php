b7ace4bb9e76e084f53eb49e71485aa85c74825e
jonkofee <jonkofee@icloud.com>
2017-08-23 18:25:23 +0300

    Валидатор для добавления нового товара

---
+++ api/validations/Good/Add.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Validator\Good;
+
+use \Phalcon\Validation\Validator\PresenceOf;
+use \Phalcon\Validation\Validator\Numericality;
+
+class Add extends \Validation
+{
+
+	public function initialize()
+	{
+		$this->add(
+			'name',
+			new PresenceOf(
+				[
+					'message' => 'Имя товара обязателен'
+				]
+			)
+		);
+
+		$this->add(
+			'bar_code',
+			new PresenceOf(
+				[
+					'message' => 'Код товара обязателен',
+					'cancelOnFail' => true
+				]
+			)
+		);
+
+		$this->add(
+			'bar_code',
+			new Numericality(
+				[
+					'message' => 'Код товара должен быть числовым значением',
+				]
+			)
+		);
+	}
+
+}
