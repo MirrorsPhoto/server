952369b5d45031885e21b1e750e661c30e568120
jonkofee <jonkofee@icloud.com>
2017-08-23 12:16:24 +0300

    Валидатор для продажи товара

---
+++ api/validations/Good/Sale.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Validator\Auth;
+
+use \Phalcon\Validation\Validator\PresenceOf;
+use \Phalcon\Validation\Validator\Numericality;
+
+class Login extends \Validation
+{
+
+	public function initialize()
+	{
+		$this->add(
+			'bar_code',
+			new PresenceOf(
+				[
+					'message' => 'Код товара обязателен',
+				]
+			)
+		);
+
+		$this->add(
+			'bar_code',
+			new Numericality(
+				[
+					'message' => 'Код товара должен быть числовым значением',
+				]
+			)
+		);
+	}
+
+}
