947de932462ce485472cd78dee1e8415680aedd4
jonkofee <jonkofee@icloud.com>
2017-08-23 12:50:16 +0300

    Изменил поведение конструктора исключений

--- Core/CoreException.php
+++ Core/CoreException.php
@@ -1,4 +1,11 @@
 <?php
 abstract class CoreException extends Exception
 {
+
+	public function __construct($message = "", $code = 0, Throwable $previous = null)
+	{
+		$this->message = $message;
+		$this->code = $code;
+	}
+
 }
