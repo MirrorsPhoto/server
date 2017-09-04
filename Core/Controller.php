82bea513a4d00f6bc41c4148b815d3b25c341605
jonkofee <jonkofee@icloud.com>
2017-09-04 10:55:27 +0300

    default null argument

--- Core/Controller.php
+++ Core/Controller.php
@@ -3,12 +3,12 @@
 abstract class Controller extends \Phalcon\Mvc\Controller
 {
 
-	protected function getQuery($name)
+	protected function getQuery($name = null)
 	{
 		return $this->request->getQuery($name);
 	}
 
-	protected function getPost($name)
+	protected function getPost($name = null)
 	{
 		return $this->request->getPost($name);
 	}
