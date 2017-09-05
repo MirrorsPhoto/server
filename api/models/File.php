0e0deb60bf591585c0b254bd586eeb1a591849d2
jonkofee <jonkofee@icloud.com>
2017-09-05 15:50:41 +0300

    Метод для фабрикации файла

--- api/models/File.php
+++ api/models/File.php
@@ -19,7 +19,47 @@ class File extends Model
      */
     public $datetime_create;
 
-    /**
+	public static function factory(Phalcon\Http\Request\File $file)
+    {
+	    $fileName = hash_file('md5', $file->getTempName()) . ".{$file->getExtension()}";
+
+	    $config = ConfigIni::getInstance();
+
+	    $dir = $config->static->dir;
+
+	    $path = "{$fileName[0]}{$fileName[1]}/{$fileName[2]}{$fileName[3]}";
+
+	    //Проверка на наличие файла
+	    if (file_exists("$dir/$path/$fileName")) {
+		    $obj = self::findFirstByPath("$path/$fileName");
+
+		    if (!$obj) {
+			    $obj = new self();
+
+			    $obj->path = "$path/$fileName";
+
+			    $obj->save();
+		    }
+
+	    } else {
+		    //Если нет такой папки - создать
+		    if (!is_dir("$dir/$path")) mkdir("$dir/$path", 0777, true);
+
+		    $isSave = $file->moveTo("$dir/$path/$fileName");
+
+		    if (!$isSave) throw new \Core\Exception\ServerError('Не удалось записать файл');
+
+		    $obj = new self();
+
+		    $obj->path = "$path/$fileName";
+
+		    $obj->save();
+	    }
+
+	    return $obj;
+    }
+
+	/**
      * Initialize method for model.
      */
     public function initialize()
