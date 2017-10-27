98ec6a2a896cf70f93cbf86af312f2cb33455c44
jonkofee <jonkofee@icloud.com>
2017-10-27 10:47:59 +0300

    Модель для городов

---
+++ api/models/City.php
@@ -0,0 +1,48 @@
+<?php
+
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\PresenceOf;
+
+class City extends Model
+{
+	protected $_tableName = 'city';
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    public $name;
+
+	/**
+	 * Initialize method for model.
+	 */
+	public function initialize()
+	{
+		parent::initialize();
+
+		//$this->hasMany('id', 'Department', 'city_id');
+	}
+
+    /**
+     * Validations and business logic
+     *
+     * @return boolean
+     */
+    public function validation()
+    {
+        $validator = new Validation();
+
+	    $validator->add(
+		    'name',
+		    new PresenceOf(
+			    [
+				    'message' => 'Название города обязательно для заполнения',
+			    ]
+		    )
+	    );
+
+        return $this->validate($validator);
+    }
+
+}
