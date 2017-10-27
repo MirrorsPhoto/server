9fd960316eae0857eac0d56f9d44e2cda5b96d32
jonkofee <jonkofee@icloud.com>
2017-10-27 10:34:23 +0300

    Миграция для таблицы с городами

---
+++ api/migrations/1.0.15/city.php
@@ -0,0 +1,64 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class CityMigration_115
+ */
+class CityMigration_115 extends Migration
+{
+	private $_tableName = 'city';
+
+	/**
+	 * Define the table structure
+	 *
+	 * @return void
+	 */
+	public function morph()
+	{
+		$this->morphTable($this->_tableName, [
+				'columns' => [
+					new Column(
+						'id',
+						[
+							'type' => Column::TYPE_INTEGER,
+							'notNull' => true,
+							'autoIncrement' => true,
+							'first' => true
+						]
+					),
+					new Column(
+						'name',
+						[
+							'type' => Column::TYPE_TEXT,
+							'notNull' => true,
+						]
+					)
+				],
+				'indexes' => [
+					new Index('city_name', ['name']),
+					new Index('city_pkey', ['id'], 'PRIMARY KEY'),
+				],
+			]
+		);
+
+
+		$this->batchInsert($this->_tableName, [
+				'name'
+			]
+		);
+	}
+
+	/**
+	 * Reverse the migrations
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		self::$_connection->dropTable($this->_tableName);
+	}
+
+}
