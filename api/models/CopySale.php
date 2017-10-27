c6198ca5210447a294297252a58442dd87a076d8
jonkofee <jonkofee@icloud.com>
2017-10-27 18:10:13 +0300

    Модель для таблицы `copy_sale`

---
+++ api/models/CopySale.php
@@ -0,0 +1,89 @@
+<?php
+
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Numericality;
+use Phalcon\Validation\Validator\PresenceOf;
+
+class CopySale extends Model
+{
+
+	protected $_tableName = 'copy_sale';
+
+	/**
+	 *
+	 * @var integer
+	 * @Column(type="integer", length=11, nullable=false)
+	 */
+	public $department_id;
+
+	/**
+	 *
+	 * @var integer
+	 * @Column(type="integer", length=11, nullable=false)
+	 */
+	public $user_id;
+
+    /**
+     *
+     * @var string
+     * @Column(type="string", nullable=false)
+     */
+    public $datetime;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("public");
+        $this->belongsTo('user_id', '\User', 'id', ['alias' => 'User']);
+    }
+
+	/**
+	 * Validations and business logic
+	 *
+	 * @return boolean
+	 */
+	public function validation()
+	{
+		$validator = new Validation();
+
+		$validator->add(
+			'user_id',
+			new Numericality(
+				[
+					'message' => 'Id пользователя должно быть числом',
+				]
+			)
+		);
+
+		$validator->add(
+			'photo_size_id',
+			new PresenceOf(
+				[
+					'message' => 'Id размера фотографии обязательное поле',
+				]
+			)
+		);
+
+		$validator->add(
+			'user_id',
+			new PresenceOf(
+				[
+					'message' => 'Id пользователя обезательное поле',
+				]
+			)
+		);
+
+		return $this->validate($validator);
+	}
+
+	public function beforeSave()
+	{
+		$user = \Core\UserCenter\Security::getUser();
+
+		$this->user_id = $user->id;
+		$this->department_id = $user->department_id;
+	}
+
+}
