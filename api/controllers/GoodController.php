90fb812e83e41dbca8ed5deb57003bb816f20661
Prischenko Sergey <jonkofee@icloud.com>
2018-05-04 18:28:44 +0300

    :fire: old code

--- api/controllers/GoodController.php
+++ api/controllers/GoodController.php
@@ -24,29 +24,7 @@ class GoodController extends Controller
 
 		$newGood->save();
 
-		return $newGood;
-	}
-
-
-	/**
-	 * @Post('/sale')
-	 */
-	public function saleAction()
-	{
-		(new \Validator\Good\Sale())->validate();
-
-		$barCode = $this->getPost('bar_code');
-
-		$good = Good::findFirstByBarCode($barCode);
-
-		if (!$good) throw new \Core\Exception\BadRequest('Товар с таким кодом отсутствует');
-
-		if (!$good->isAvailable()) throw new \Core\Exception\BadRequest('Такого товара нет в наличии');
-
-		$good->sale();
-
-		return 'Товар успешно продан';
-
+		return $newGood->refresh();
 	}
 
 	/**
